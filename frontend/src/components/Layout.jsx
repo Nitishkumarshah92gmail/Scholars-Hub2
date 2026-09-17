6e17cda08c8916961be64d8fe73f15030c4d02ea
Nitish Kumar Sahu <nitishkumarshah92@gmail.com>
2026-09-17 22:21:49 +0530

    style: remove 700px max-width constraint in main layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -540,7 +540,7 @@ export default function Layout() {
           </NavLink>
         </div>
 
-        <div className="max-w-[700px] mx-auto px-4 md:px-8 py-8 md:py-12">
+        <div className="max-w-6xl mx-auto px-4 md:px-8 py-8 md:py-12 w-full">
           <Outlet />
         </div>
       </main>
